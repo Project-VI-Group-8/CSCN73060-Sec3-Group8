59e30a5c3799ac1a9659dd58339c3d90d70ed56f
Saiful Islam <saiful.i9489@gmail.com>
2026-02-11 14:23:41 -0500

    Add database context (AppDbContext)

---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,46 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data;
+
+public class AppDbContext : DbContext
+{
+	public AppDbContext(DbContextOptions options) : base(options) {}
+
+	public DbSet<Product> Products => Set<Product>();
+	public DbSet<Order> Orders => Set<Order>();
+	public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+	public DbSet<Payment> Payments => Set<Payment>();
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		// Products
+		modelBuilder.Entity<Product>()
+			.HasIndex(p => p.Name);
+
+		// Order -> OrderItems (1 to many)
+		modelBuilder.Entity<OrderItem>()
+			.HasOne(oi => oi.Order)
+			.WithMany(o => o.Items)
+			.HasForeignKey(oi => oi.OrderId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		// OrderItem -> Product (many to 1)
+		modelBuilder.Entity<OrderItem>()
+			.HasOne(oi => oi.Product)
+			.WithMany()
+			.HasForeignKey(oi => oi.ProductId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		// Order -> Payment (1 to 1)
+		modelBuilder.Entity<Payment>()
+			.HasOne(p => p.order)
+			.WithOne(o => o.Payment)
+			.HasForeignKey<Payment>(p => p.OrderId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		modelBuilder.Entity<Payment>()
+			.HasIndex(p => p.OrderId)
+			.IsUnique();
+	}
+}
